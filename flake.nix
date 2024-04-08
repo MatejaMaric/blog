4db4490922aa1386c618a61fbd1fcb86b97b064d
Mateja <mail@matejamaric.com>
2024-04-08 22:45:29 +0200

    feat(flake): implement default overlay

--- flake.nix
+++ flake.nix
@@ -7,26 +7,29 @@
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
       nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
+      drv = {stdenv, hugo, ...}: stdenv.mkDerivation {
+        name = pkgName;
+        src = ./.;
+        buildInputs = [ hugo ];
+        buildPhase = ''
+          hugo
+        '';
+        installPhase = ''
+          mkdir -p $out/var/www
+          cp -r public $out/var/www/matejamaric.com
+        '';
+      };
     in
     {
+      overlays.default = (final: prev: {
+        matejasblog = prev.callPackage drv {};
+      });
       packages = forAllSystems (system:
         let
           pkgs = nixpkgsFor.${system};
-          drv = pkgs.stdenv.mkDerivation {
-            name = pkgName;
-            src = ./.;
-            buildInputs = [ pkgs.hugo ];
-            buildPhase = ''
-              hugo
-            '';
-            installPhase = ''
-              mkdir -p $out/var/www
-              cp -r public $out/var/www/matejamaric.com
-            '';
-          };
         in {
-          ${pkgName} = drv;
-          default = drv;
+          ${pkgName} = drv pkgs;
+          default = drv pkgs;
         }
       );
       devShells = forAllSystems (system:
