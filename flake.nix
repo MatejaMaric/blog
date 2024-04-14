5e5197f5d24d5f131c443e10aa916903824a830e
Mateja <mail@matejamaric.com>
2024-04-14 13:42:23 +0200

    feat(flake): add my cv package to the build process

--- flake.nix
+++ flake.nix
@@ -1,13 +1,18 @@
 {
   description = "Nix Flake package for my blog";
   inputs.nixpkgs.url = "nixpkgs/nixos-23.11";
-  outputs = { self, nixpkgs }:
+  inputs.matejascv.url = "github:/MatejaMaric/cv";
+  inputs.matejascv.inputs.nixpkgs.follows = "nixpkgs";
+  outputs = { self, nixpkgs, matejascv }:
     let
       pkgName = "matejasblog";
       supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
       forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
-      nixpkgsFor = forAllSystems (system: import nixpkgs { inherit system; });
-      drv = {stdenv, hugo, ...}: stdenv.mkDerivation {
+      nixpkgsFor = forAllSystems (system: import nixpkgs {
+        inherit system;
+        overlays = [ matejascv.overlays.default ];
+      });
+      drv = {stdenv, hugo, matejascv}: stdenv.mkDerivation {
         name = pkgName;
         src = ./.;
         nativeBuildInputs = [ hugo ];
@@ -17,29 +22,22 @@
         installPhase = ''
           mkdir -p $out/var/www
           cp -r public $out/var/www/matejamaric.com
+          cp ${matejascv} $out/var/www/matejamaric.com/cv.pdf
         '';
       };
     in
     {
       overlays.default = (final: prev: {
-        ${pkgName} = prev.callPackage drv {};
+        ${pkgName} = prev.callPackage drv { inherit matejascv; };
       });
-      packages = forAllSystems (system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in {
-          ${pkgName} = drv pkgs;
-          default = drv pkgs;
-        }
-      );
-      devShells = forAllSystems (system:
-        let
-          pkgs = nixpkgsFor.${system};
-        in {
-          default = pkgs.mkShell {
-            buildInputs = with pkgs; [ git rsync hugo ];
-          };
-        }
-      );
+      packages = forAllSystems (system: {
+        ${pkgName} = nixpkgsFor.${system}.callPackage drv {};
+        default = nixpkgsFor.${system}.callPackage drv {};
+      });
+      devShells = forAllSystems (system: {
+        default = nixpkgsFor.${system}.mkShell {
+          buildInputs = with nixpkgsFor.${system}; [ git rsync hugo ];
+        };
+      });
     };
 }
